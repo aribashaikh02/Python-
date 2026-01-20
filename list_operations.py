ad90f182eddfcd325cc5018baac41d5b96f9f784
ARIBA SHAIKH <shaikhariba0203@gmail.com>
2026-01-20 22:31:56 +0530

    Add files via upload

---
+++ list_operations.py
@@ -0,0 +1,14 @@
+nums= [10,20,40,70,80]
+
+#access
+print ("First Element: ", nums[0])
+print ("Last element: ", nums[-1])
+
+#update
+nums [1]=200
+print ("After update: ", nums)
+
+#delete
+del nums[2]
+nums.remove(70)
+print("After deletion: ", nums)
