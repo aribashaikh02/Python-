1c0dcebbbad4ccde604c85427501fce2c8746079
ARIBA SHAIKH <shaikhariba0203@gmail.com>
2026-01-21 20:15:34 +0530

    Add variable declarations and print types in var.py

---
+++ sem1/datatype/var.py
@@ -0,0 +1,16 @@
+name = "Ariba"          
+age = 18               
+height = 5.4          
+is_student = True       
+hobbies = ["Reading", "Swimming", "Coding"] 
+address = ("Mumbai", "India")               
+grades = {"Math": 90, "English": 85}       
+
+
+print("Name:", name, "| Type:", type(name))
+print("Age:", age, "| Type:", type(age))
+print("Height:", height, "| Type:", type(height))
+print("Is student:", is_student, "| Type:", type(is_student))
+print("Hobbies:", hobbies, "| Type:", type(hobbies))
+print("Address:", address, "| Type:", type(address))
+print("Grades:", grades, "| Type:", type(grades))
