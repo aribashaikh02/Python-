ec0f5cb37ffdbe3321f22e8fdacb888420bbc0a7
ARIBA SHAIKH <shaikhariba0203@gmail.com>
2026-01-29 23:05:40 +0530

    Fix bubble sort implementation for ascending order

--- DSA/sorting/bubble/bub.asc.py
+++ DSA/sorting/bubble/bub.asc.py
@@ -5,8 +5,8 @@ arr = [5, 3, 4, 1, 2]
 n=len(arr)
 
 for i in range(n-1):
-    for j in range(1, n-i):
-        if arr[j-1] < arr[j]:
-            arr[j-1], arr[j] = arr[j], arr[j-1]
+    for j in range(0, n-i-1):
+        if arr[j] > arr[j+1]:
+            arr[j], arr[j+1] = arr[j+1], arr[j]
 
 print(arr)
