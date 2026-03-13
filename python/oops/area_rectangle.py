35f4d78e3ce6bca04f2e373137ef90320789f5a7
ARIBA SHAIKH <shaikhariba0203@gmail.com>
2026-03-13 16:46:26 +0530

    Add Rectangle class to calculate area

---
+++ python/oops/area_rectangle.py
@@ -0,0 +1,11 @@
+#finding the area of rectanlge
+class Rectangle:
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def display_area(self):
+        print(f"The area of the rectangle is: {self.base * self.height} square units.")
+
+new_rectangle = Rectangle(12,10)
+new_rectangle.display_area()
