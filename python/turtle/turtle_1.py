2aa8b386a1522f955007f742b19b793bf5d86969
ARIBA SHAIKH <shaikhariba0203@gmail.com>
2026-03-13 16:59:53 +0530

    Add basic turtle graphics example

---
+++ python/turtle/turtle_1.py
@@ -0,0 +1,13 @@
+# import turtle graphics module 
+import turtle 
+
+# create turtle to do your bidding 
+my_turtle = turtle.Turtle()
+my_turtle.color("red")
+my_turtle.shape("turtle")
+
+# your turtle awaits your command 
+my_turtle.forward(100)
+
+#this statement (or an equivalent) must be the last statement in your program
+turtle.done()
